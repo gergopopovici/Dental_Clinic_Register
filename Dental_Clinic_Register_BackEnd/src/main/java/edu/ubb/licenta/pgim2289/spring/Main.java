19f1dc3103a3924031e007fa3737f961536f7c85
gergopopovici <popovicigergo0330@gmail.com>
2025-07-01 12:55:06 +0300

    feat: add security and mail dependencies; update application configuration for JWT and email settings

--- Dental_Clinic_Register_BackEnd/src/main/java/edu/ubb/licenta/pgim2289/spring/Main.java
+++ Dental_Clinic_Register_BackEnd/src/main/java/edu/ubb/licenta/pgim2289/spring/Main.java
@@ -1,10 +1,13 @@
 package edu.ubb.licenta.pgim2289.spring;
 
 
+import edu.ubb.licenta.pgim2289.spring.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
