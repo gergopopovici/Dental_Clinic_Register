c620bb1e42ac5fcce9230b08adb25c25c416e1a8
gergopopovici <popovicigergo0330@gmail.com>
2025-07-01 12:11:31 +0300

    feat: integrate react-query for data fetching and add MUI icon type declarations

--- Dental_Clinic_Register_FrontEnd/src/App.tsx
+++ Dental_Clinic_Register_FrontEnd/src/App.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
+import Register from './pages/Register';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { PublicRoute } from './components/PublicRoute';
+import PageNotFound from './pages/PageNotFound';
+import EmailConfirmation from './pages/EmailConfirmation';
 
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path={"/login"} element={Login()}/>
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify" element={<EmailConfirmation />} />
+        </Route>
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<div>Dashboard Page (Coming Soon)</div>} />
+          <Route path="/" element={<div>Home Page (Coming Soon)</div>} />
+        </Route>
+
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Dental_Clinic_Register_FrontEnd/src/mui-icon.d.ts
@@ -0,0 +1,7 @@
+declare module '@mui/icons-material/*' {
+  import { SvgIconProps } from '@mui/material/SvgIcon';
+  import { ComponentType } from 'react';
+
+  const Icon: ComponentType<SvgIconProps>;
+  export default Icon;
+}
