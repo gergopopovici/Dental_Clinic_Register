ad031260b15f19340122212e00a24ede4edf3178
gergopopovici <popovicigergo0330@gmail.com>
2025-08-07 18:07:36 +0300

    feat: add custom exceptions for BraceComponent and TreatmentPlan; update GlobalExceptionHandler to handle them

---
+++ Dental_Clinic_Register_BackEnd/src/main/java/edu/ubb/licenta/pgim2289/spring/exception/TreatmentPlanException.java
@@ -0,0 +1,7 @@
+package edu.ubb.licenta.pgim2289.spring.exception;
+
+public class TreatmentPlanException extends RuntimeException {
+    public TreatmentPlanException(String message) {
+        super(message);
+    }
+}
